a2c66e303b94ef51c8faa82ad9e506317696e158
Kim Brown <kbrownc341@gmail.com>
2021-05-23 13:00:11 -0400

    Empty board created

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, TouchableOpacity } from 'react-native';
+import { newBoard } from './newboard.js';
 
 export default function App() {
 
   const nav = [
-    { key: 1, name: 'Play' }, 
+    { key: 1, name: 'Play', invisible: false }, 
     { key: 2, name: '', invisible: true }, 
     { key: 3, name: '', invisible: true }, 
     { key: 4, name: '', invisible: true },
@@ -13,29 +14,54 @@ export default function App() {
     { key: 7, name: '0' },
   ];
 
-  const board = [
-    { key: 1, name: 'End' }, 
-    { key: 2, name: '', invisible: true }, 
-    { key: 3, name: '', invisible: true }, 
-    { key: 4, name: 'Roll' },
-    { key: 5, name: '', invisible: true }, 
-    { key: 6, name: '', invisible: true }, 
-    { key: 7, name: 'Start' },
-  ];
-
   const numColumns = 7;
 
-  renderItem = ({ item }) => {
+  const renderNav = ({ item }) => {
     if (item.invisible === true) {
       return <View style={[styles.item, styles.itemInvisible]} />;
     }
+    if (item.name === 'Play') {
+      return (
+        <View style={styles.item}>
+        <TouchableOpacity onPress={() => pressPlay(item.key)}> 
+                <Text style={styles.itemText}>{item.name}</Text>
+                </TouchableOpacity>
+        </View>
+        )
+    }
     return (
-      <View
-        style={styles.item}
-      >
+      <View style={styles.item}>
+          <Text style={styles.itemText}>{item.name}</Text>
+      </View>
+    );
+  };
+
+  const renderBoard = ({ item }) => {
+    if (item.invisible === true) {
+      return <View style={[styles.item, styles.itemInvisible]} />;
+    }
+    if (item.name === 'Roll') {
+      return (
+        <View style={styles.item}>
+        <TouchableOpacity onPress={() => pressRoll(item.key)}> 
+                <Text style={styles.itemText}>{item.name}</Text>
+                </TouchableOpacity>
+        </View>
+        )
+    }
+    return (
+      <View style={styles.item}>
         <Text style={styles.itemText}>{item.name}</Text>
       </View>
     );
+  };
+
+  const pressPlay = (key) => {
+    console.log('Play', key);
+  };
+
+  const pressRoll = (key) => {
+    console.log('Roll', key);
   };
 
   return (
@@ -43,8 +69,8 @@ export default function App() {
         <View style={styles.nav}>
             <FlatList
               data={nav}
-              renderItem={renderItem}
-              style={styles.container}
+              renderItem={renderNav}
+              style={styles.nav}
               numColumns={numColumns}
             />
             <View style={styles.message}>
@@ -54,8 +80,8 @@ export default function App() {
         </View>
         <View style={styles.board}>
             <FlatList
-              data={board}
-              renderItem={renderItem}
+              data={newBoard}
+              renderItem={renderBoard}
               style={styles.board}
               numColumns={numColumns}
             />
@@ -71,14 +97,17 @@ const styles = StyleSheet.create({
   },
   nav: {
     flex: 2,
+    fontSize: 10,
+    fontWeight: 'bold',
+    marginVertical: 1,
   },
   item: {
-    backgroundColor: '#4D243D',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
     margin: 1,
-    height: Dimensions.get('window').width / 7, // approximate a square
+    height: Dimensions.get('window').width / 15,
   },
   itemInvisible: {
     backgroundColor: 'transparent',
@@ -96,8 +125,8 @@ const styles = StyleSheet.create({
   },
   board: {
     flex: 7,
-    color: 'black',
     fontSize: 10,
     fontWeight: 'bold',
+    marginVertical: 5,
   }
 });
